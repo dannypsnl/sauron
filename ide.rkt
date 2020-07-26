95c1c7e452f384aa953c16d8b8a5185bad21cbaf
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-07-27 07:42:06 +0800

    (#1) open/save existed file

--- ide.rkt
+++ ide.rkt
@@ -15,22 +15,40 @@
   ; but editors are created as instances of text% or pasteboard%.
   (define text (new text%))
 
-  (define menu-bar (new menu-bar% [parent ide]))
+  (define m-bar (new menu-bar% [parent ide]))
+  (define m-file (new menu% [label "File"] [parent m-bar]))
+  (define mi-open
+    (new menu-item%
+         [label "Open"]
+         [parent m-file]
+         [callback
+          (λ (i e)
+            (define path (get-file #f ide))
+            (when path
+              (send text load-file path 'text)))]
+         [shortcut #\o]
+         [shortcut-prefix (get-default-shortcut-prefix)]))
+  (define mi-save
+    (new menu-item%
+         [label "Save"]
+         [parent m-file]
+         [callback
+          (λ (i e)
+            (send text save-file #f 'text))]
+         [shortcut #\s]
+         [shortcut-prefix (get-default-shortcut-prefix)]))
   (append-editor-operation-menu-items
-   (new menu%
-        [label "Edit"]
-        [parent menu-bar]) #f)
-  (append-editor-font-menu-items
-   (new menu%
-        [label "Font"]
-        [parent menu-bar]))
+   (new menu% [label "Edit"] [parent m-bar]) #f)
   (send editor set-editor text)
 
+  (pre-insert-text text)
+  (send text set-max-undo-history 100)
+
+  (send ide show #t))
+
+(define (pre-insert-text text)
   (define pre-inserted #<<EOS
 #lang racket
 EOS
     )
-  (send text insert (make-object string-snip% pre-inserted))
-  (send text set-max-undo-history 100)
-
-  (send ide show #t))
+  (send text insert (make-object string-snip% pre-inserted)))
