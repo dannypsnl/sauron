4c0ad0b2c29e60dd26ca1a7afbb05a26ded66c2a
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-07-27 14:52:56 +0800

    update dependencies

--- info.rkt
+++ info.rkt
@@ -1,7 +1,11 @@
 #lang info
 (define collection "sauron")
-(define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define deps '("base"
+               "gui-lib"))
+(define build-deps '("scribble-lib"
+                     "racket-doc"
+                     "gui-doc"
+                     "rackunit-lib"))
 (define scribblings '(("scribblings/sauron.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
