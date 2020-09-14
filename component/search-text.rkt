52a744dd9586353e23fe166a321f2c3d3fcf82a4
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-09-14 17:15:00 +0800

    can move cursor via enter/shift+enter now

--- component/search-text.rkt
+++ component/search-text.rkt
@@ -12,17 +12,19 @@
       will-do-nothing-with)
     (init-field search-on)
 
+    (define cur-search? #f)
+    (define search-result* '())
+
     (define/override (on-char e)
       (match (send e get-key-code)
         [#\return #:when (will-do-nothing-with #\return)
-                  ; TODO: move cursor to next/prev occurs
-                  (if (send e get-shift-down)
-                      (void)
-                      (void))]
+                  (if (and (send e get-shift-down) cur-search?)
+                      (set-pos (λ (n) (- n 1)))
+                      (set-pos (λ (n) (+ n 1))))]
         [#\backspace (super on-char e)
-                     (if (not (= (string-length (get-text)) 0))
-                       (search (get-text))
-                       (search #f))]
+                     (if (not (= 0 (string-length (get-text))))
+                         (search (get-text))
+                         (search #f))]
         [key-code #:when (not (member key-code control-key-list))
                   (super on-char e)
                   ; NOTE:
@@ -30,11 +32,27 @@
                   ; if this fail, check control-key-list, if might lack of something
                   (search (get-text))]
         [else (super on-char e)]))
+
+    (define/private (set-pos f)
+      (displayln cur-search?)
+      (when cur-search?
+        (when (< cur-search? 0)
+          (set! cur-search? (- (length search-result*) 1)))
+        (when (>= cur-search? (length search-result*))
+          (set! cur-search? 0))
+        (send search-on set-position (list-ref search-result* cur-search?)
+              (+ (string-length (get-text))
+                 (list-ref search-result* cur-search?)))
+        (set! cur-search? (f cur-search?))))
     (define/private (search search-target)
       (send* search-on
         [set-searching-state search-target #f #f #t]
         [finish-pending-search-work])
-      (displayln (send search-on get-search-bubbles)))))
+      (set! search-result* (sort (map (λ (e)
+                                        (caar e))
+                                      (send search-on get-search-bubbles))
+                                 <))
+      (set! cur-search? (if (= 0 (length search-result*)) #f 0)))))
 
 (module+ main
   (define test-frame (new frame%
