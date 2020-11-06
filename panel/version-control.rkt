dc01dc97ade9fba001f7066f4b0b3c4bbd6a336d
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-11-06 12:19:50 +0800

    add commit message editor into version control panel

--- panel/version-control.rkt
+++ panel/version-control.rkt
@@ -1,9 +1,27 @@
 #lang racket/gui
 
+(require "../component/common-editor.rkt")
+
+(define version-controller%
+  (class vertical-panel%
+    (init-field project-folder)
+
+    (super-new)
+
+    (define editor-canvas (new editor-canvas%
+                           [parent this]
+                           [style '(no-hscroll)]))
+    (define commit-message-editor (new common:text%))
+    (send editor-canvas set-editor commit-message-editor)))
+
 (module+ main
   (define test-frame (new frame%
                           [label "Version Control Panel"]
-                          [width 1200]
+                          [width 300]
                           [height 600]))
+
+  (define vc (new version-controller%
+                  [parent test-frame]
+                  [project-folder ""]))
 
   (send test-frame show #t))
