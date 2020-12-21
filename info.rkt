e44c0af9125fb930982fb794c01b4a3e7fd789ec
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-12-22 07:32:33 +0800

    [info] allow multi-page and add category

--- info.rkt
+++ info.rkt
@@ -4,7 +4,7 @@
                "drracket-tool-lib" "sandbox-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"
                      "gui-doc"))
-(define scribblings '(("scribblings/sauron.scrbl" ())))
+(define scribblings '(("scribblings/sauron.scrbl" (multi-page) (tool))))
 (define pkg-desc "A Racket IDE")
 (define version "0.2.1")
 (define pkg-authors '(dannypsnl))
