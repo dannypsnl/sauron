ed675a9a272d6be7521de3ab8a507db1b2063ab5
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-01-01 13:53:51 +0800

    fix for macos for now

--- shortcut.rkt
+++ shortcut.rkt
@@ -1,10 +1,7 @@
 #lang s-exp framework/keybinding-lang
 
-;;; ref: https://docs.racket-lang.org/gui/Editor_Functions.html#%28def._%28%28lib._mred%2Fmain..rkt%29._map-command-as-meta-key%29%29
-(map-command-as-meta-key #t)
-
 ;;; delete whole thing from current position to the start of line
-(keybinding "m:backspace" (lambda (editor event)
+(keybinding "d:backspace" (lambda (editor event)
   (define end (send editor get-start-position))
   (define line (send editor position-line end))
   (define start (send editor line-start-position line))
