d540b06958faeaac542ff0f7d1ca91fb19eef491
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-02-11 18:31:35 +0800

    commit pusher

--- commit-pusher.rkt
+++ commit-pusher.rkt
@@ -5,31 +5,29 @@
 (require framework
          "execute-cmd.rkt")
 
-(define commit-pusher%
-  (class frame%
-    (super-new [label "Push Commits"] [width 600] [height 600])
-
-    (define logs (make-parameter ""))
-    (run "git log fetch_head..head --oneline"
-         (λ (out in err)
-           (logs out)))
-
-    (define editor-canvas (new editor-canvas%
-                               [parent this]
-                               [style '(no-hscroll)]))
-    (define vc-logs (new racket:text%))
-    (send editor-canvas set-editor vc-logs)
-
-    (send vc-logs insert (logs))
-
-    (define push-button (new button% [parent this]
-                             [label "Push"]))
-    ))
+(define (commit-pusher% parent)
+  (run "git log fetch_head..head --oneline"
+       (λ (out in err)
+         (define logs
+           (string-join (for/list ([line (in-lines out)])
+                          line)
+                        "\n"))
+         (define result
+           (message-box/custom "Push Commits"
+                               logs
+                               "push"
+                               "cancel"
+                               #f
+                               parent))
+         (match result
+           [1 (run "git push")]
+           [2 (void)]))))
 
 (module+ main
   (require "project-manager.rkt")
 
   (current-project (current-directory))
 
-  (define pusher (new commit-pusher%))
-  (send pusher show #t))
+  (define frame (new frame% [label "test-commit-pusher"]))
+
+  (commit-pusher% frame))
