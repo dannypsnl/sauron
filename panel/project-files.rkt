291ea6b22dbe6543f3d94aa6d4cb4dcb92c8609b
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-05-16 00:17:48 +0800

    [project] FIX delete, argument error

--- panel/project-files.rkt
+++ panel/project-files.rkt
@@ -123,7 +123,7 @@ modifier author: Lîm Tsú-thuàn(GitHub: @dannypsnl)
          [label "remove"]
          [callback (λ (btn event)
                      (define path (send viewer get-cur-selected-file))
-                     (delete-directory/files path #:must-exists? #f)
+                     (delete-directory/files path #:must-exist? #f)
                      (send viewer reset-directory (send current-project get)))])))
 
 (module+ main
