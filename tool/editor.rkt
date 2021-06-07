7ad09225c85eebf59507b71a99ec56e7cdf49983
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-07 21:04:20 +0800

    [editor] giveup number and operator invoke

--- tool/editor.rkt
+++ tool/editor.rkt
@@ -20,9 +20,7 @@
         (define/override (on-char e)
           (match (send e get-key-code)
             [(or #\a #\b #\c #\d #\e #\f #\g #\h #\i #\j #\k #\l #\m #\n #\o #\p #\q #\r #\s #\t #\u #\v #\w #\x #\y #\z
-                 #\A #\B #\C #\D #\E #\F #\G #\H #\I #\J #\K #\L #\M #\N #\O #\P #\Q #\R #\S #\T #\U #\V #\W #\X #\Y #\Z
-                 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9 #\0
-                 #\+ #\- #\* #\/)
+                 #\A #\B #\C #\D #\E #\F #\G #\H #\I #\J #\K #\L #\M #\N #\O #\P #\Q #\R #\S #\T #\U #\V #\W #\X #\Y #\Z)
              #:when (not (send e get-meta-down))
              (send this auto-complete)
              (super on-char e)]
