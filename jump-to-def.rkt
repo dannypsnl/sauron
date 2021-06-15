20ea0b23318269f79ebaa8f6cc6723c216ad16e7
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-16 00:19:40 +0800

    [editor] change to tab

--- jump-to-def.rkt
+++ jump-to-def.rkt
@@ -9,5 +9,8 @@
   (define b? (send editor jump-to-def from-pos))
   (when b?
     (match-define (binding text start end filename) b?)
-    (define ed (send (send (send (send editor get-tab) get-frame) find-matching-tab filename) get-defs))
+    (define frame (send (send editor get-tab) get-frame))
+    (define tab (send frame find-matching-tab filename))
+    (define ed (send tab get-defs))
+    (send frame change-to-tab tab)
     (send ed set-position start end)))
