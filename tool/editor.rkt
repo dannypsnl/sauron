9ad6ccc5bca68218f295adba578689d1c3c61922
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-16 20:19:39 +0800

    [dev] simplify

--- tool/editor.rkt
+++ tool/editor.rkt
@@ -33,36 +33,35 @@
         (define bindings (make-interval-map))
         (define defs (make-hash))
         (define/public (update-env)
-          (let ([filename (src)])
-            ;;; TODO: show-content reports error via exception, catch it and show
-            (for ([e (show-content filename)])
-              (match e
-                [(vector syncheck:add-docs-menu start end id _ document-page _ _)
-                 (interval-map-set! doc start (add1 end) document-page)]
-                [(vector syncheck:add-arrow/name-dup/pxpy
-                         start-left start-right _ _
-                         end-left end-right _ _
-                         actual? level require-arrow? name-dup?)
-                 (define id (string->symbol (send this get-text end-left end-right)))
-                 (define loc
-                   (if require-arrow?
-                       (let ([path (send this get-text start-left start-right)])
-                         ;; get tab: from-path
-                         (define tab (send (send (send this get-tab) get-frame) find-matching-tab path))
-                         (if tab
-                             (let ([ed (send tab get-defs)])
-                               (send ed update-env)
-                               (send ed get-def id))
-                             #f))
-                       (binding id start-left start-right (src))))
-                 (when loc
-                   (interval-map-set! bindings end-left (add1 end-right)
-                                      loc))]
-                [(vector syncheck:add-definition-target start end id style-name)
-                 (hash-set! defs id
-                            (binding id start end (src)))]
-                [(vector syncheck:add-jump-to-definition start end id filename submods)
-                 (void)]
-                [else (void)]))))))
+          ;;; TODO: show-content reports error via exception, catch it and show
+          (for ([e (show-content (src))])
+            (match e
+              [(vector syncheck:add-docs-menu start end id _ document-page _ _)
+               (interval-map-set! doc start (add1 end) document-page)]
+              [(vector syncheck:add-arrow/name-dup/pxpy
+                       start-left start-right _ _
+                       end-left end-right _ _
+                       actual? level require-arrow? name-dup?)
+               (define id (string->symbol (send this get-text end-left end-right)))
+               (define loc
+                 (if require-arrow?
+                     (let ([path (send this get-text start-left start-right)])
+                       ;; get tab: from-path
+                       (define tab (send (send (send this get-tab) get-frame) find-matching-tab path))
+                       (if tab
+                           (let ([ed (send tab get-defs)])
+                             (send ed update-env)
+                             (send ed get-def id))
+                           #f))
+                     (binding id start-left start-right (src))))
+               (when loc
+                 (interval-map-set! bindings end-left (add1 end-right)
+                                    loc))]
+              [(vector syncheck:add-definition-target start end id style-name)
+               (hash-set! defs id
+                          (binding id start end (src)))]
+              [(vector syncheck:add-jump-to-definition start end id filename submods)
+               (void)]
+              [else (void)])))))
 
     (drracket:get/extend:extend-definitions-text drracket-editor-mixin)))
