c3b664e31baaea78d6bec148678ad0c6fe0b125c
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-20 16:17:10 +0800

    [dev] struct test case to show how to use it

--- collect/binding.rkt
+++ collect/binding.rkt
@@ -2,4 +2,12 @@
 
 (provide (struct-out binding))
 
-(struct binding (name start end filename))
+(struct binding
+  (name start end filename)
+  #:transparent)
+
+(module+ test
+  (require rackunit)
+
+  (check-equal? (binding "fake" 0 1 "fake.rkt")
+                (binding "fake" 0 1 "fake.rkt")))
