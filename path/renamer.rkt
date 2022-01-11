c07676e8f77d73546547a4e37053b9f169e4aa88
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-01-11 13:25:52 +0800

    log and use `find-files`

--- path/renamer.rkt
+++ path/renamer.rkt
@@ -4,24 +4,19 @@
 
 (require racket/gui
          framework/preferences
+         "../log.rkt"
          "../collect/api.rkt")
 
 (define (auto-rename old-path new-path)
   (define current-project-dir (preferences:get 'current-project))
-  (recur-on-dir current-project-dir old-path new-path)
+  (define racket-files (find-files (lambda (p) (path-has-extension? p #".rkt"))))
+  (for ([f racket-files])
+    (define to-update-loc (require-location? f old-path))
+    (when to-update-loc
+      (match-define (list start end) to-update-loc)
+      (define t (new text%))
+      (send t load-file f)
+      (send t insert (path->string (find-relative-path (path-only f) new-path)) start end)
+      (send t save-file)
+      (log:debug "~a get updated, since ~a get renamed to ~a" f old-path new-path)))
   (rename-file-or-directory old-path new-path))
-
-(define (recur-on-dir root old-path new-path)
-  (for ([f (directory-list root)])
-    (cond
-      [(directory-exists? f)
-       (recur-on-dir f old-path new-path)]
-      [(file-exists? f)
-       (define to-update-loc (require-location? f old-path))
-       (when to-update-loc
-         (match-define (list start end) to-update-loc)
-         (define t (new text%))
-         (send t load-file f)
-         (send t insert (path->string (find-relative-path root new-path)) start end)
-         (send t save-file))]
-      [else (void)])))
