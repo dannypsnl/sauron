06900d73b3545cf6b5f07b762b295b7f50dd2438
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-02-25 06:47:40 +0800

    [script] for linux/windows

--- scripts/rename-identifier.rkt
+++ scripts/rename-identifier.rkt
@@ -4,10 +4,21 @@
 (define (send-command command editor)
   (send (send editor get-keymap) call-function command editor (new event%) #t))
 
-;;; c+r rename identifier
-(define-script rename-identifier
+(define (impl event #:editor editor)
+  (send-command "Rename Identifier" editor))
+
+(define-script rename-identifier-macos
                #:label "rename identifier"
                #:menu-path ("sauron")
                #:shortcut-prefix (cmd)
                #:shortcut #\r
-               (λ (event #:editor editor) (send-command "Rename Identifier" editor)))
+               #:os-types (macosx)
+               (λ (event #:editor editor) (impl event #:editor editor)))
+
+(define-script rename-identifier
+               #:label "rename identifier"
+               #:menu-path ("sauron")
+               #:shortcut-prefix (ctl)
+               #:shortcut #\r
+               #:os-types (unix windows)
+               (λ (event #:editor editor) (impl event #:editor editor)))
