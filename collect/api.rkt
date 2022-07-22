518761ecc75284ccaa786639129ce7ba6fadc5f9
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-07-22 10:21:15 +0800

    by providing `do-nothing` thread, the invalid path get handled
    issue close #201

--- collect/api.rkt
+++ collect/api.rkt
@@ -3,7 +3,12 @@
          update)
 (require data/interval-map
          sauron/collect/record
-         sauron/collect/cache)
+         sauron/collect/record-maintainer)
+
+;;; tell corresponding maintainer update the record
+(define (update path)
+  (thread-send (get-record-maintainer path)
+               (list 'update)))
 
 (define (require-location? path require)
   (match-define (struct* record ([requires requires]))
@@ -21,3 +26,9 @@
   (match-define (struct* record ([defs defs]))
     (get-record path))
   (hash-ref defs id #f))
+
+;;; try get record from maintainer map via path
+(define (get-record path)
+  (thread-send (get-record-maintainer path)
+               (list 'get-record (current-thread)))
+  (thread-receive))
--- collect/record-maintainer.rkt
+++ collect/record-maintainer.rkt
@@ -1,7 +1,11 @@
-#lang racket
+#lang racket/base
 (provide get-record-maintainer
          terminate-record-maintainer)
-(require sauron/collect/record
+(require racket/path
+         racket/function
+         racket/match
+         data/interval-map
+         sauron/collect/record
          sauron/collect/collector
          sauron/log)
 
@@ -16,22 +20,38 @@
   (kill-thread maintainer)
   (hash-set! path=>maintainer path #f))
 
+;;; this thread do nothing and provide fake reply is need
+; the purpose is making sure the caller will fail gratefully, but no need to handle exception
+; this is because the caller already think cannot fetch data is normal
+; in editor, users can always try to get jump to definition even no definition exists
+; so caller will just ignore the operation, thus, another error handling shouldn't be there
+(define do-nothing (thread (thunk (let loop ()
+                                    (match (thread-receive)
+                                      [(list 'get-record from)
+                                       (thread-send from (record 0
+                                                                 (make-interval-map)
+                                                                 (make-interval-map)
+                                                                 (make-hash)
+                                                                 (make-hash)))]
+                                      [else (void)])
+                                    (loop)))))
+
 (define (make-record-maintainer file-path)
-  (unless (file-exists? file-path)
-    (let/ec return
-      (log:warning "update non-existed path: ~a" file-path)
-      (return #f)))
-  (define cached-record (collect-from file-path))
-  (thread
-   (thunk
-    (let loop ()
-      (match (thread-receive)
-        [(list 'update)
-         (match-define (struct* record ([created-time created-time])) cached-record)
-         (when (< created-time (file-or-directory-modify-seconds file-path))
-           (set! cached-record (collect-from file-path)))
-         (loop)]
-        ;; to invoke this, you must provide your thread-id as from
-        [(list 'get-record from)
-         (thread-send from cached-record)
-         (loop)])))))
+  (if (and (file-exists? file-path) (path-has-extension? file-path #".rkt"))
+      (let ([cached-record (collect-from file-path)])
+        (thread
+         (thunk
+          (let loop ()
+            (match (thread-receive)
+              [(list 'update)
+               (match-define (struct* record ([created-time created-time])) cached-record)
+               (when (< created-time (file-or-directory-modify-seconds file-path))
+                 (set! cached-record (collect-from file-path)))]
+              ;; to invoke this, you must provide your thread-id as from
+              [(list 'get-record from)
+               (thread-send from cached-record)])
+            (loop)))))
+      ; when path is invalid, return same thread to handle all non-sense requirement
+      ; since only one-more thread here, it should not be a big overhead
+      (begin (log:warning "cannot create maintainer for invalid path: ~a" file-path)
+             do-nothing)))
