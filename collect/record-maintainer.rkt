974fc9bf8b35a2887969d5f756f88c7d86be02e6
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-08-04 20:46:13 +0800

    every maintainer will notify those it is depending
    TODO
    - CHECK: does `mutable-set` takes too much memory?
    - FEATURE: the maintainer collects references from `depender`
    
    related to #157
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- collect/record-maintainer.rkt
+++ collect/record-maintainer.rkt
@@ -2,9 +2,11 @@
 (provide create-record-maintainer
          get-record-maintainer
          terminate-record-maintainer)
-(require racket/path
+(require racket/future
          racket/function
          racket/match
+         racket/path
+         racket/set
          data/interval-map
          sauron/collect/record
          sauron/collect/collector
@@ -15,6 +17,8 @@
        (path-has-extension? file-path #".rkt")))
 
 (define path=>maintainer (make-hash))
+(define (maybe-create-maintainer path)
+  (hash-ref! path=>maintainer path (thunk (make-record-maintainer path))))
 ;;; The constraint is only `record-maintainer-creator` allowed to create new maintainer
 ; but anyone might like to create one concurrently, so `hash-ref!` here blocked all repeated creation
 ; since `thread-receive` ensure every creation is proceeded one by one
@@ -24,8 +28,12 @@
    (thunk
     (let loop ()
       (match (thread-receive)
+        [(list 'wait future path)
+         (define mt (maybe-create-maintainer path))
+         (thread-send mt
+                      (list 'depended (touch future)))]
         [(list 'create from path)
-         (define mt (hash-ref! path=>maintainer path (thunk (make-record-maintainer path))))
+         (define mt (maybe-create-maintainer path))
          ; `from` must be another thread
          (when from
            (thread-send from mt))])
@@ -34,6 +42,10 @@
 (define (create-record-maintainer path [from #f])
   (thread-send record-maintainer-creator
                (list 'create from path)))
+
+(define (future-wait-record-maintainer path future)
+  (thread-send record-maintainer-creator
+               (list 'wait future path)))
 
 (define (get-record-maintainer path #:wait? [wait? #f])
   (cond
@@ -72,13 +84,25 @@
 (define (make-record-maintainer file-path)
   (thread
    (thunk
+    (define self (current-thread))
+    (define (notify-others record)
+      (for/async ([path (hash-keys (record-requires record))])
+        (future-wait-record-maintainer path (current-future))
+        self))
+
     (define cached-record (collect-from file-path))
+    (notify-others cached-record)
+    (define depender (mutable-set))
+
     (let loop ()
       (match (thread-receive)
         [(list 'update)
          (match-define (struct* record ([created-time created-time])) cached-record)
-         (when (< created-time (file-or-directory-modify-seconds file-path))
-           (set! cached-record (collect-from file-path)))]
+         (when (created-time . < . (file-or-directory-modify-seconds file-path))
+           (set! cached-record (collect-from file-path))
+           (notify-others cached-record))]
+        [(list 'depended from)
+         (set-add! depender from)]
         ;; to invoke this, you must provide your thread-id as from
         [(list 'get-record from)
          (thread-send from cached-record)])
