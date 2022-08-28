cfda0460c85bcd10f52c3c9d1cee68d1cbe33293
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-08-28 14:16:34 +0800

    [dep] goblins
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- info.rkt
+++ info.rkt
@@ -6,6 +6,7 @@
            "data-lib"
            "drracket-plugin-lib"
            "drracket-tool-lib"
+           "goblins"
            "file-watchers"
            "raco-invoke"
            ; syntax
