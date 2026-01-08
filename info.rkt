1b7f1135886b4cc7f85307f291a7d1f44fb549bf
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2026-01-08 21:19:59 +0800

    deps: rakka

--- info.rkt
+++ info.rkt
@@ -7,6 +7,8 @@
            "drracket-plugin-lib"
            "drracket-tool-lib"
            "raco-invoke"
+           ; runtime
+           "rakka"
            ; syntax
            "try-catch-finally-lib"
            "curly-fn-lib"
